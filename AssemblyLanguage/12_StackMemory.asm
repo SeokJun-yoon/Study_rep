b70e237e81fc787873b8a3c43b8a20d452edd699
Yoon Seok Jun <70671442+SeokJun-yoon@users.noreply.github.com>
2024-02-15 05:29:38 +0900

    240215_stackmemory, C++ start
    Study about stackmemory and setting for studying CPP

--- AssemblyLanguage/12_StackMemory.asm
+++ AssemblyLanguage/12_StackMemory.asm
@@ -11,7 +11,7 @@ CMAIN:
     ; - a b c d 범용 레지스터
     ; - 포인터 레지스터 (포인터 = 위치를 가리키는~)
     ; -- ip (Instruction Pointer) : 다음 수행 명령어의 위치
-    ; -- sp (Stack Pointer) : 현재 스택 top 위치 (일종의 cursor)
+    ; -- sp (Stack Pointer) : 현재 스택 top 위치 (일종의  cursor)
     ; -- bp (Base Pointer) : 스택 상대주소 계산용
     
     push rax
