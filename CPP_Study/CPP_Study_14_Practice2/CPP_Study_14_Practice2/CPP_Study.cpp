8ce114c64429dadf4d4a520a35ebda52e6d7fbb3
Yoon Seok Jun <70671442+SeokJun-yoon@users.noreply.github.com>
2024-04-21 00:12:09 +0900

    Finish Practice 2 Project
    달팽이 연습문제 마무리.
    직접 푼 문제 + 효율적인 풀이.
    효울화 된 코드 방식 잘 기억하기

--- CPP_Study/CPP_Study_14_Practice2/CPP_Study_14_Practice2/CPP_Study.cpp
+++ CPP_Study/CPP_Study_14_Practice2/CPP_Study_14_Practice2/CPP_Study.cpp
@@ -94,11 +94,97 @@ void PrintBoard()
 #pragma endregion
 
 #pragma region 강사님 풀이
+
+enum DIR
+{
+	RIGHT=0,
+	DOWN=1,
+	LEFT=2,
+	UP=3,
+};
+
+bool CanGo(int y, int x)
+{
+	if (y < 0 || y >= N)
+		return false;
+	if (x < 0 || x >= N)
+		return false;
+	if (board[y][x] != 0)
+		return false;
+	return true;
+}
+
 void SetBoard()
 {
+	int dir = RIGHT;
+	int num = 1;
+	int y = 0;
+	int x = 0;
 
+	int dy[] = { 0,1,0,-1 };
+	int dx[] = { 1,0,-1,0 };
+
+
+	while (true)
+	{
+		board[y][x] = num;
+
+		if (num == N * N)
+			break;
+
+		int nextY = y + dy[dir];
+		int nextX = x + dx[dir];
+
+		// dy[], dx[]를 쓰게 된다면 굳이 switch(dir) 부분이 필요가 없어짐.
+		// 코드 효율화 시키기
+		//switch (dir)
+		//{
+		//case RIGHT:
+		//	nextY = y;
+		//	nextX = x + 1;
+		//	break;
+		//case DOWN:
+		//	nextY = y + 1;
+		//	nextX = x;
+		//	break;
+		//case LEFT:
+		//	nextY = y;
+		//	nextX = x - 1;
+		//	break;
+		//case UP:
+		//	nextY = y - 1;
+		//	nextX = x;
+		//	break;
+		//}
+
+		if (CanGo(nextY, nextX))
+		{
+			y = nextY;
+			x = nextX;
+			num++;
+		}
+		else
+		{
+			dir = (dir + 1) % 4;	// 코드를 효율적으로 짤 수 있다.
+			/*switch (dir)
+			{
+			case RIGHT:
+				dir = DOWN;
+				break;
+			case DOWN:
+				dir = LEFT;
+				break;
+			case LEFT:
+				dir = UP;
+				break;
+			case UP:
+				dir = RIGHT;
+				break;
+			}*/
+		}
+
+	}
 }
-#pragma endregion
 
 int main()
 {
@@ -111,3 +197,5 @@ int main()
 
 	return 0;
 }
+
+#pragma endregion
