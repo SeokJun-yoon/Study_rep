8a7fbf92fade0b7b88bc8583bacf1c274e6163ed
Yoon Seok Jun <70671442+SeokJun-yoon@users.noreply.github.com>
2024-04-21 00:55:23 +0900

    240421 OOP Start
    객체지향 프로그래밍 강의 시작.

--- CPP_Study/CPP_Study_15_File_Dividing/CPP_Study_15_File_Dividing/Test1.h
+++ CPP_Study/CPP_Study_15_File_Dividing/CPP_Study_15_File_Dividing/Test1.h
@@ -1,5 +1,11 @@
 //#pragma once
 // 중복되더라도 한 번만 읽도록 한다.
+// pragma once가 최적화 하기에 좋다.
+
+// 아래 #ifndef - #define - #endif 문법도 읽을 줄은 알아야한다.
+// 기능은 중복 방지로 같다.
+
+// 헤더 파일에 쓸데없는 너무 많은 것을 넣지 말 것.
 
 #ifndef _TEST1_H__
 #define _TEST1_H__
---
+++ CPP_Study/CPP_Study_16_OOP_Start/CPP_Study_16_OOP_Start/CPP_Study.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// 오늘의 주제 : 객체 지향의 시작
+
+// 객체지향 프로그래밍
+// 절차(procedure)지향 프로그래밍
+// - procedure = 함수
+
+
+int main()
+{
+
+
+
+	return 0;
+}
