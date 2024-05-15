fdba86d4f276d7eff04187a175228c2bb4efa1fd
Yoon Seok Jun <70671442+SeokJun-yoon@users.noreply.github.com>
2024-05-16 04:37:07 +0900

    섹션 6.5 다형성 업데이트
    가상함수, 순수 가상함수 내용 학습.

--- CPP_Study2/6_5_Polymorphism/6_5_Polymorphism/CPP_Study.cpp
+++ CPP_Study2/6_5_Polymorphism/6_5_Polymorphism/CPP_Study.cpp
@@ -19,13 +19,31 @@ using namespace std;
 // 일반 함수는 정적 바인딩을 사용
 // 동적 바인딩을 원한다면? -> 가상 함수 (virtual function)
 
+// 그런데 실제 객체가 어떤 타입인지 어떻게 알고 알아서 가상함수를 호출해준걸까?
+// - 가상 함수 테이블 (vftable)
+
+// .vftable [] 4바이트(32) 8바이트(64)
+// [VMove] [VDie]
+
+// 순수 가상 함수 : 구현은 없고 '인터페이스'만 전달하는 용도로 사용하고 싶을 경우
+// 추상 클래스 : 순수 가상 함수가 1개 이상 포함되면 바로 추상 클래스로 간주
+// - 직접적으로 객체를 만들 수 없게 됨
+
 class Player
 {
 public:
+	Player()
+	{
+		_hp = 100;
+	}
 	void Move() { cout << "Move Player !" << endl; }
 
 	//void Move(int a) { cout << "Move Player (int) ! " << endl; }
-	virtual void VMove() { cout << "Move Player !" << endl; }
+	virtual void VMove() { cout << "VMove Player !" << endl; }
+	virtual void VDie() { cout << "VDie Player !" << endl; }
+
+	// 순수 가상 함수
+	virtual void VAttack() = 0;	
 
 public:
 	int _hp;
@@ -34,11 +52,20 @@ public:
 class Knight : public Player
 {
 public:
+	Knight()
+	{
+		_stamina = 100;
+	}
+
 	void Move() { cout << "Move Knight!" << endl; }
 	// 같은 형태의 함수를 만들어서 호출하면 상속받은 함수가 아닌 위 함수가 호출된다.
 	
 	// 가상 함수는 재정의를 하더라도 가상 함수다!
 	virtual void VMove() { cout << "Move Knight !" << endl; }
+	virtual void VDie() { cout << "VDie Knight !" << endl; }
+
+	virtual void VAttack() { cout << "VAttack Knight" << endl; }
+
 
 public:
 	int _stamina;
@@ -65,7 +92,7 @@ void MoveKnight(Knight* knight)
 
 int main()
 {
-	Player p;
+	//Player p;
 	//MovePlayer(&p); // 플레이어는 플레이어다? YES
 	//MoveKnight(&p); // 플레이어는 기사다? NO
 	//p.Move();
