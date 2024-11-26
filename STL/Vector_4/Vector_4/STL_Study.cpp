48b5a4d8c0c5db5fc702430e4f4eb88f1edf7b27
SeokJun-yoon <70671442+SeokJun-yoon@users.noreply.github.com>
2024-11-26 15:43:23 +0900

    241126_vector 내용 복습
    vector 개념 복습, 직접 class 구현 및 부가 기능 직접 구현 연습

---
+++ STL/Vector_4/Vector_4/STL_Study.cpp
@@ -0,0 +1,193 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+// 오늘의 주제 : vector
+
+
+template<typename T>
+class Iterator
+{
+public:
+	Iterator() : _ptr(nullptr)
+	{
+
+	}
+
+	Iterator(T* ptr) : _ptr(ptr)
+	{
+
+	}
+
+	// 전위형
+	Iterator& operator++()
+	{
+		_ptr++;
+		return *this;
+	}
+
+	// 후위형
+	Iterator operator++(int)
+	{
+		Iterator temp = *this;
+		_ptr++;
+		return temp;
+	}
+
+	// 전위형
+	Iterator& operator--()
+	{
+		_ptr--;
+		return *this;
+	}
+
+	// 후위형
+	Iterator operator--(int)
+	{
+		Iterator temp = *this;
+		_ptr--;
+		return temp;
+	}
+
+	Iterator operator+(const int count)
+	{
+		Iterator temp = *this;
+		temp._ptr += count;
+		return temp;
+	}
+
+	Iterator operator-(const int count)
+	{
+		Iterator temp = *this;
+		temp._ptr -= count;
+		return temp;
+	}
+
+
+	// boolian operator
+	bool operator==(const Iterator& right)
+	{
+		return _ptr == right._ptr;
+	}
+
+	bool operator!=(const Iterator& right)
+	{
+		return _ptr != right._ptr;
+	}
+
+	T& operator*()
+	{
+		return *_ptr;
+	}
+
+public:
+	T* _ptr;
+};
+
+
+template<typename T>
+class Vector 
+{
+public:
+
+	Vector() : _data(nullptr), _size(0), _capacity(0)
+	{
+
+	}
+
+	~Vector()
+	{
+		if (_data)
+			delete[] _data;
+	}
+
+	void push_back(const T& val)
+	{
+		if (_size == _capacity)
+		{
+			// 증설 작업
+			int newCapacity = static_cast<int>(_capacity * 1.5);
+
+			if (newCapacity == _capacity)
+				newCapacity++;
+
+			reserve(newCapacity);
+		}
+
+		// 데이터 저장
+		_data[_size] = val;
+
+		// 데이터 개수 증가
+		_size++;
+	}
+
+	void reserve(int capacity)
+	{
+		_capacity = capacity;
+
+		T* newData = new T[_capacity];
+
+		// 데이터 복사
+		if (_data)
+		{
+			for (int i = 0; i < _size; i++)
+				newData[i] = _data[i];
+		}
+
+		// 기존에 있던 데이터를 날린다
+		if (_data)
+			delete[] _data;
+
+		// 교체
+		_data = newData;
+	}
+
+	T& operator[](const int pos) { return _data[pos]; }
+
+	int size() { return _size; }
+	int capacity() { return _capacity; }
+
+public:
+	typedef Iterator<T> iterator;
+
+	void clear() { _size = 0; }
+	iterator begin() { return iterator(&_data[0]); }
+	iterator end() { return begin() + _size; }
+
+private:
+	T* _data;
+	int _size;
+	int _capacity;
+};
+
+
+int main()
+{
+	// 만든 클래스 Vector
+	Vector<int> v;
+
+	// 기본 vector
+	 //vector<int> v;
+	 
+	 
+	v.reserve(100);
+
+	for (int i = 0; i < 100; i++)
+	{
+		v.push_back(i);
+		cout << v.size() << " " << v.capacity() << endl;
+	}
+
+	for (int i = 0; i < v.size(); i++)
+	{
+		cout << v[i] << endl;
+	}
+
+	cout << "---------------------------------" << endl;
+
+	for (Vector<int>::iterator it = v.begin(); it != v.end(); ++it)
+	{
+		cout << (*it) << endl;
+	}
+
+	return 0;
+}
