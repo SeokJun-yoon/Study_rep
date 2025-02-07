459d6fe6d4028d743fb3f35af33490b161d8735e
SeokJun-yoon <70671442+SeokJun-yoon@users.noreply.github.com>
2025-02-07 23:51:11 +0900

    Cache
    캐시에 대한 간단한 실습, 캐시 히트로 인한 성능 차이 확인

--- GameServer_Study/Server/ServerCore/Program.cs
+++ GameServer_Study/Server/ServerCore/Program.cs
@@ -6,46 +6,33 @@ namespace ServerCore
 {
     class Program
     {
-        volatile static bool _stop = false;  // 전역으로 선언된 변수는, main과 ThreadMain 모두에서 접근이 가능
-        // volatile을 앞에 붙이면, 최적화를 하지 말라는 의미
-
-        // volatile는 C++과 C#에서 약간 다른 부분이 있는데,
-        // 둘 다 최적화를 하지 말아달라는 의미는 같지만
-        // C#는 조금 더 추가적인 내용이 있다.
-        
-        // 그래서 사실 volatile 보다는 추후의 memory barrier나 lock, atomic 등을 사용하는 것이 좋다.
-
-        static void ThreadMain()
-        {
-            Console.WriteLine("쓰레드 시작!");
-
-            while (_stop==false)
-            {
-                // 누군가가 stop 신호를 해주기를 기다린다.
-            }
-
-            Console.WriteLine("쓰레드 종료!");
-        }
-
         static void Main(string[] args)
         {
-            Task t = new Task(ThreadMain);
-            t.Start();
+            int[,] arr = new int[10000, 10000];
+            {
+                long now = DateTime.Now.Ticks;
+                for (int y = 0; y < 10000; y++)
+                    for (int x = 0; x < 10000; x++)
+                        arr[y, x] = 1;
+                long end = DateTime.Now.Ticks;
+                Console.WriteLine($"(y, x) 순서 걸린 시간 {end - now}");
 
-            Thread.Sleep(1000);
+            }
+            // 일반적인 생각으로는 위와 아래의 시간이 같아야 한다.
+            {
+                long now = DateTime.Now.Ticks;
+                for (int y = 0; y < 10000; y++)
+                    for (int x = 0; x < 10000; x++)
+                        arr[x, y] = 1;
+                long end = DateTime.Now.Ticks;
+                Console.WriteLine($"(x, y) 순서 걸린 시간 {end - now}");
+            }
 
-            _stop = true;
-
-            Console.WriteLine("Stop 호출");
-            Console.WriteLine("종료 대기중");
-
-            t.Wait();   // Thread 사용시에는 join과 같음
-            // Release 모드로 실행 시, Wait 상태를 빠져나오지 못함.
-            // 멀티쓰레드 환경에서는 이런 일이 발생할 수 있다.
-
-
-            Console.WriteLine("종료 성공");
+            // 하지만 실행해보면 꽤나 큰 시간 차이가 난다.
+            // 첫 번째 경우, 인접한 메모리 공간 때문에 캐시 히트가 일어나는데
+            // 두 번재 경우, 인접하지 않은 부분에 대한 접근이므로 시간이 느려지는 것이다.
         }
+
     }
 }
 
